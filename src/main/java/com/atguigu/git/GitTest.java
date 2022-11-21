9921eb0ae7af24b8a66cab3f18311ddd5e1734ee
hezixiu <3111018965@qq.com>
2022-11-21 20:41:43 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
     }
 }
